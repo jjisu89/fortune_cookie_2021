e3be9e1613f24dd4ea78553fc17a8314fbf42cf7
jisu_jeong <jisujeong@jisu-jeongui-MacBookPro.local>
2021-12-05 01:27:52 +0900

    updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import './Navigation.css';
 function Navigation() {
   return (
     <div className="nav">
-      <Link to="/">오늘의 포춘쿠키</Link>
+      {/* <Link to="/">오늘의 포춘쿠키</Link> */}
       <Link to="/open">더 열기</Link>
       {/* <Link to="/foryou">For you</Link>
       <Link to="/about">About</Link> */}
